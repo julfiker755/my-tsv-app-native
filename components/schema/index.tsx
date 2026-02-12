02b2a52b3b3253c5f0ea0db1530895f3e50239e8
julfiker755 <julfiker755.bd@gmail.com>
2026-02-13 00:30:12 +0600

    otp design screen implement

--- components/schema/index.tsx
+++ components/schema/index.tsx
@@ -1,12 +1,35 @@
 import * as Yup from "yup";
 
-const email = Yup.string()
-  .email("Invalid email address")
-  .required("Email is required");
-
+export const email_sc = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+});
 export const Login_sc = Yup.object().shape({
-  email: email,
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
     .required("Password is required"),
+});
+
+export const register_sc = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  c_password: Yup.string()
+    .oneOf([Yup.ref("password")], "Passwords do not match")
+    .required("Confirm password is required"),
+});
+
+export const otp_sc = Yup.object().shape({
+  otp: Yup.string()
+    .matches(/^\d+$/, "OTP must be numeric")
+    .length(4, `OTP must be 4 digits`)
+    .required("OTP is required"),
 });
